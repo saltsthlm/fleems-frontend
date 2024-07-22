b4aec275e18cef0184ca9abe93866043028d2db1
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-22 15:27:22 +0200

    create FormButton component
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

---
+++ src/components/FormButton.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from "react";
+
+type FormButtonProps = {
+  onClick: () => unknown;
+};
+export default function FormButton({
+  children,
+  onClick: callback,
+}: PropsWithChildren<FormButtonProps>) {
+  return (
+    <button onClick={callback} className="btn text-font bg-button">
+      {children}
+    </button>
+  );
+}
