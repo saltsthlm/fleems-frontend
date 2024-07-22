90798ed27fae57a8b296f1274c06e67a8e68fe63
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-22 15:26:28 +0200

    add custom colors
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        button: "#E7E8EC",
+        background: "#F5F5F5",
+        secondary: "#FFFFFF",
+        font: "#000000",
+      },
+    },
   },
   plugins: [daisyui],
 };
