1e8ab8800582ad2e86adc3b9abe76b6cd8f6b3b7
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-22 17:01:32 +0200

    improve styling
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,43 +1,47 @@
 import { Link } from "@tanstack/react-router";
-import { useState } from "react";
+import { SyntheticEvent, useState } from "react";
 import FormButton from "./FormButton";
 
 export default function LoginForm() {
   const [email, setEmail] = useState<string>();
   const [password, setPassword] = useState<string>();
 
-  const submitForm = () => {
+  const submitForm = (e: SyntheticEvent) => {
+    e.preventDefault();
     console.log("Logging in...");
   };
 
   return (
-    <div>
-      <form
-        onSubmit={submitForm}
-        className="card bg-background text-black p-10"
-      >
+    <form onSubmit={submitForm} className="flex flex-col gap-10 items-center">
+      <div className="card bg-background text-black p-10 w-full drop-shadow-lg gap-3">
         <label htmlFor="email">Email address</label>
         <input
           name="email"
           type="text"
-          className="p-3 rounded"
+          className="p-3 rounded rounded-xl"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-
         <label htmlFor="password">Password</label>
         <input
           name="password"
           type="password"
-          className="p-3 rounded"
+          className="p-3 rounded rounded-xl"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-      </form>
-      <FormButton onClick={submitForm}>Login</FormButton>
-      <p>
-        Not a member? <Link to="/register">Register here</Link>
-      </p>
-    </div>
+      </div>
+      <div className="w-full flex flex-col items-center gap-3">
+        <FormButton className="w-3/5">
+          <strong>LOGIN</strong>
+        </FormButton>
+        <p>
+          Not a member?{" "}
+          <strong>
+            <Link to="/register">Register here</Link>
+          </strong>
+        </p>
+      </div>
+    </form>
   );
 }
