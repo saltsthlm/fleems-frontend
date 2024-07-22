4570fdddb4306543f060f6cbd1287d9320321b58
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-22 17:01:05 +0200

    allow for classnames
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/FormButton.tsx
+++ src/components/FormButton.tsx
@@ -2,13 +2,18 @@ import { PropsWithChildren } from "react";
 
 type FormButtonProps = {
   onClick?: () => unknown;
+  className?: string;
 };
 export default function FormButton({
   children,
   onClick: callback,
+  className,
 }: PropsWithChildren<FormButtonProps>) {
   return (
-    <button onClick={callback} className="btn text-font bg-button">
+    <button
+      onClick={callback}
+      className={`btn text-font bg-button border-0 ${className}`}
+    >
       {children}
     </button>
   );
