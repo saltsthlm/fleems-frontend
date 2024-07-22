90a7a38a858a65b0dcd1a274a77b1112405e8196
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-22 16:23:58 +0200

    add logo
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,11 @@
 import LoginForm from "../components/LoginForm";
+import Logo from "../components/Logo";
 import PageWithNavigation from "../components/PageWithNavigation";
 
 export default function LoginPage() {
   return (
     <PageWithNavigation showNav={false}>
+      <Logo />
       <LoginForm />
     </PageWithNavigation>
   );
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,8 +1,10 @@
+import Logo from "../components/Logo";
 import PageWithNavigation from "../components/PageWithNavigation";
 
 export default function RegisterPage() {
   return (
     <PageWithNavigation showNav={false}>
+      <Logo />
       <h1>Register</h1>
     </PageWithNavigation>
   );
