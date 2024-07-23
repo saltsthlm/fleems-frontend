31a72a98dd89a1cff88721380dea1ead89ca4063
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-23 08:55:56 +0200

    change styling to match figma design's layout
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,9 +2,9 @@ import logo from "../assets/logo.png";
 
 export default function Logo() {
   return (
-    <div>
-      <img src={logo}/>
-      <p>Trucking made easy</p>
+    <div className="p-10">
+      <img src={logo} className="w-full"/>
+      <p className="text-center">Trucking made easy</p>
     </div>
   );
 }
