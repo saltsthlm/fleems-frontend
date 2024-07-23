6a4a652ab5a0a204b8b74e0a11494f746a5611a1
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-23 09:19:15 +0200

    linting
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,5 +5,5 @@ export default function Card({ children }: PropsWithChildren) {
     <div className="card bg-background text-black p-5 w-full drop-shadow-lg gap-3">
       {children}
     </div>
-  )
+  );
 }
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png";
 export default function Logo() {
   return (
     <div className="p-10">
-      <img src={logo} className="w-full"/>
+      <img src={logo} className="w-full" />
       <p className="text-center">Trucking made easy</p>
     </div>
   );
