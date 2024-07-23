73249adb175f0e9a5710f809a9745eceff59ec86
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-23 15:58:44 +0200

    add some additional styling options
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/CardButton.tsx
+++ src/components/CardButton.tsx
@@ -1,16 +1,20 @@
 import { PropsWithChildren } from "react";
+import { PropsWithClassName } from "../types/ComponentTypes";
 
-type CardButtonProps = {
+interface CardButtonProps extends PropsWithClassName {
   onClick?: () => unknown;
-};
+  isCentered?: boolean;
+}
 export default function CardButton({
   children,
   onClick: callback,
+  isCentered = true,
+  className,
 }: PropsWithChildren<CardButtonProps>) {
   return (
     <button
       onClick={callback}
-      className="card bg-background text-black p-5 w-full drop-shadow-lg gap-3 items-center justify-center py-10"
+      className={`card bg-background text-black p-5 w-full drop-shadow-lg gap-3 ${className} ${isCentered && "items-center justify-center py-10"}`}
     >
       {children}
     </button>
