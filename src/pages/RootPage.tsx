88f9024b18fbcb3ab49f255ba7d4665ae4c3bfe2
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-23 16:49:23 +0200

    remove left over heading
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -3,7 +3,6 @@ import { Outlet } from "@tanstack/react-router";
 export default function RootPage() {
   return (
     <div className="min-h-screen bg-secondary text-font text-sm flex flex-col relative">
-      <h1>helloooo</h1>
       <Outlet />
     </div>
   );
