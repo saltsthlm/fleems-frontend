865dc77148ebbc7bfe0c6f12640d514fe629babd
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-23 09:43:05 +0200

    make vehicles page content mirror drivers page content
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/VehiclesPage.tsx
+++ src/pages/VehiclesPage.tsx
@@ -1,10 +1,21 @@
+import CardButton from "../components/CardButton";
+import CardLink from "../components/CardLink";
+import GapList from "../components/GapList";
 import PageHeading from "../components/PageHeading";
 import PageWithNavigation from "../components/PageWithNavigation";
 
 export default function VehiclesPage() {
+  const createTruck = () => {
+    console.log("Adding truck...");
+  }
+
   return (
     <PageWithNavigation>
       <PageHeading>Vehicles</PageHeading>
+      <GapList>
+        <CardButton onClick={createTruck}>+ Add new truck</CardButton>
+        <CardLink to="/">Fleet information</CardLink>
+      </GapList>
     </PageWithNavigation>
   );
 }
