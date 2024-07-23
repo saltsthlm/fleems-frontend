84ec63e217d25166eca51d03d7b3d920085b7cd3
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-23 12:00:40 +0200

    start structuring "drivers" page
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/DriversPage.tsx
+++ src/pages/DriversPage.tsx
@@ -1,21 +1,61 @@
+import { useState } from "react";
 import CardButton from "../components/CardButton";
-import CardLink from "../components/CardLink";
 import GapList from "../components/GapList";
 import PageHeading from "../components/PageHeading";
 import PageWithNavigation from "../components/PageWithNavigation";
+import CreateDriverForm from "../components/CreateDriverForm";
 
 export default function DriversPage() {
+  const [creatingDriver, setCreatingDriver] = useState<boolean>(false);
+  const [showingDrivers, setShowingDrivers] = useState<boolean>(false);
+
+  const resetPage = () => {
+    setShowingDrivers(false);
+    setCreatingDriver(false);
+  };
+
   const createDriver = () => {
+    setShowingDrivers(false);
+    setCreatingDriver(true);
     console.log("Creating driver...");
+  };
+
+  const showDrivers = () => {
+    setCreatingDriver(false);
+    setShowingDrivers(true);
   };
 
   return (
     <PageWithNavigation>
-      <PageHeading>Drivers</PageHeading>
-      <GapList>
-        <CardButton onClick={createDriver}>+ Add new driver</CardButton>
-        <CardLink to="/">Driver information</CardLink>
-      </GapList>
+      {!creatingDriver && !showingDrivers && (
+        <>
+          <PageHeading>Drivers</PageHeading>
+          <GapList>
+            <CardButton onClick={createDriver}>+ Add new driver</CardButton>
+            <CardButton onClick={showDrivers}>Driver information</CardButton>
+          </GapList>
+        </>
+      )}
+      {creatingDriver && (
+        <>
+          <PageHeading>
+            <button onClick={resetPage}>&lt; Add a new Driver</button>
+          </PageHeading>
+          <GapList>
+            <CreateDriverForm />
+          </GapList>
+        </>
+      )}
+      {showingDrivers && (
+        <>
+          <PageHeading>
+            <button onClick={resetPage}>&lt; Driver information</button>
+          </PageHeading>
+          <GapList>
+            <h1>Driver</h1>
+          </GapList>
+        </>
+      )}
     </PageWithNavigation>
   );
 }
