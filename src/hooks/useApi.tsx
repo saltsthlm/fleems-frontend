5e07cfcf0d608594cc9a225d80bdb0e4d8e6e462
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-23 13:56:56 +0200

    create useApi custom hook
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

---
+++ src/hooks/useApi.tsx
@@ -0,0 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { Driver } from "../types/ApiResponses";
+
+type ApiEndpoints = "drivers" | "dwaddwa";
+
+export default function useApi(endpoint: ApiEndpoints) {
+  const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
+
+  const fetchDrivers = async () => {
+    try {
+      const data = await axios.get<Driver[]>(BASE_API_URL + endpoint);
+      return data.data;
+    } catch (error) {
+      if (error instanceof AxiosError != true) {
+        console.log("An unknown error ocurred when fetching api:", error);
+        return;
+      }
+      console.log("An error ocurred when fetching api:", error.message);
+      return error;
+    }
+  };
+
+  const {
+    data,
+    isPending: loading,
+    error,
+  } = useQuery({
+    queryKey: [`api-${endpoint}`],
+    queryFn: async () => {
+      switch (endpoint) {
+        case "drivers":
+          return fetchDrivers();
+        default:
+          return null;
+      }
+    },
+  });
+
+  return { data, loading, error };
+}
