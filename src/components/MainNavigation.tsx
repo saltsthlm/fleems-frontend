17784e96917d152f6df1c1d31c960c71d65d028e
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-23 14:47:28 +0200

    add "Home" button
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -3,10 +3,15 @@ import building from "../assets/icons/building.svg";
 import checklist from "../assets/icons/checklist.svg";
 import truck from "../assets/icons/truck.svg";
 import truck_driver from "../assets/icons/truck_driver.svg";
+import house from "../assets/icons/house.svg";
 
 export default function MainNavigation() {
   return (
     <footer className="mt-auto flex justify-around bg-button py-2 text-sm text-center items-end [&>a>img]:mx-auto [&>a>img]:h-7">
+      <Link to="/">
+        <img src={house} className="py-1" />
+        Home
+      </Link>
       <Link to="/drivers">
         <img src={truck_driver} />
         Drivers
