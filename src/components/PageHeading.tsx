e0e6ea3e74878968c9ca8248626b17d6f0fdb357
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-23 09:39:57 +0200

    linting
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -1,9 +1,5 @@
 import { PropsWithChildren } from "react";
 
 export default function PageHeading({ children }: PropsWithChildren) {
-  return (
-    <h1 className="pb-5 text-xl">
-      {children}
-    </h1>
-  )
+  return <h1 className="pb-5 text-xl">{children}</h1>;
 }
