830bc446103bde18733a42ae965f9fc55d836c25
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-23 14:56:52 +0200

    allow for additional classNames
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,9 @@
 import { PropsWithChildren } from "react";
+import { PropsWithClassName } from "../types/ComponentTypes";
 
-export default function Card({ children }: PropsWithChildren) {
+export default function Card({ children, className }: PropsWithChildren<PropsWithClassName>) {
   return (
-    <div className="card bg-background text-black p-5 w-full drop-shadow-lg gap-3">
+    <div className={`card bg-background text-black p-5 w-full drop-shadow-lg gap-3 ${className}`}>
       {children}
     </div>
   );
