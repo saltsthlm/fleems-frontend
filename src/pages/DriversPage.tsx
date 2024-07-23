24ab218f06e7d7d6fe24d77d59915fba93f9c7bb
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-23 09:40:38 +0200

    add buttons to create new driver and to display driver information
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/DriversPage.tsx
+++ src/pages/DriversPage.tsx
@@ -1,10 +1,21 @@
+import CardButton from "../components/CardButton";
+import CardLink from "../components/CardLink";
+import GapList from "../components/GapList";
 import PageHeading from "../components/PageHeading";
 import PageWithNavigation from "../components/PageWithNavigation";
 
 export default function DriversPage() {
+  const createDriver = () => {
+    console.log("Creating driver...");
+  };
+
   return (
     <PageWithNavigation>
       <PageHeading>Drivers</PageHeading>
+      <GapList>
+        <CardButton onClick={createDriver}>+ Add new driver</CardButton>
+        <CardLink to="/">Driver information</CardLink>
+      </GapList>
     </PageWithNavigation>
   );
 }
