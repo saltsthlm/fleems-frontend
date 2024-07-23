fc684c868b06e02d9bb7845b3ba22b49ff787aee
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-23 14:22:40 +0200

    decrease font size
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "@tanstack/react-router";
 
 export default function RootPage() {
   return (
-    <div className="min-h-screen bg-secondary text-font flex flex-col">
+    <div className="min-h-screen bg-secondary text-font text-sm flex flex-col">
       <Outlet />
     </div>
   );
