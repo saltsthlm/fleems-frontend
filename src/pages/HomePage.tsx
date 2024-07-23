017c5ff3b33703bc2f15bc668cc821b5138237af
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-23 09:19:07 +0200

    add stats to dashboard (home page)
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,18 @@
+import FleetDataStats from "../components/FleetDataStats";
+import FleetEventsStats from "../components/FleetEventsStats";
+import GapList from "../components/GapList";
 import PageWithNavigation from "../components/PageWithNavigation";
+import TopDriversStats from "../components/TopDriversStats";
 
 export default function HomePage() {
   return (
     <PageWithNavigation>
-      <h1>Home</h1>
+      <h1>Dashboard</h1>
+      <GapList>
+        <FleetEventsStats />
+        <TopDriversStats />
+        <FleetDataStats />
+      </GapList>
     </PageWithNavigation>
   );
 }
