b114a90bebd203d4f0f3fa1a88018d86d27a69ac
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-24 16:17:44 +0200

    add ClientPage content
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/ClientsPage.tsx
+++ src/pages/ClientsPage.tsx
@@ -1,10 +1,48 @@
+import { useState } from "react";
+import Card from "../components/Card";
+import GapList from "../components/GapList";
 import PageHeading from "../components/PageHeading";
 import PageWithNavigation from "../components/PageWithNavigation";
+import useApi from "../hooks/useApi";
+import { Client } from "../types/ApiResponses";
+import { Icon } from "@iconify/react";
 
 export default function ClientsPage() {
+  const [searchFilter, setSearchFilter] = useState<string>();
+
+  const { data, loading } = useApi("clients");
+
   return (
     <PageWithNavigation>
       <PageHeading>Clients</PageHeading>
+      <div className="py-2 flex justify-end">
+        <div className="bg-button rounded-full py-1 px-3 flex">
+          <span className="pr-2">
+            <Icon
+              icon="material-symbols-light:search"
+              className="h-full"
+            ></Icon>
+          </span>
+          <input
+            type="text"
+            value={searchFilter}
+            onChange={(e) => setSearchFilter(e.target.value)}
+            placeholder="Search"
+            className="bg-transparent"
+          />
+        </div>
+      </div>
+      {loading && <h1>Loading...</h1>}
+      <GapList>
+        {data?.map((client: Client) => (
+          <Card>
+            <h1 className="text-xl">{client.name}</h1>
+            <h2>Num of tasks: {client.tasks.length}</h2>
+            <h2>Completed: {client.tasks.length}</h2>
+            <h2>Ongoing: {client.tasks.length}</h2>
+          </Card>
+        ))}
+      </GapList>
     </PageWithNavigation>
   );
 }
