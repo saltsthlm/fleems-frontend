59ecde2a9a3653ef236ee178a4355ee14de74c6b
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-24 15:44:53 +0200

    change VehiclesPage to mirror content and logic of DriversPage
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/VehiclesPage.tsx
+++ src/pages/VehiclesPage.tsx
@@ -1,21 +1,53 @@
+import { useState } from "react";
 import CardButton from "../components/CardButton";
-import CardLink from "../components/CardLink";
 import GapList from "../components/GapList";
 import PageHeading from "../components/PageHeading";
 import PageWithNavigation from "../components/PageWithNavigation";
+import TrucksList from "../components/TrucksList";
+import CreateTruckForm from "../components/CreateTruckForm";
 
 export default function VehiclesPage() {
+  const [creatingTruck, setCreatingTruck] = useState<boolean>(false);
+  const [showingTruck, setShowingTruck] = useState<boolean>(false);
+
+  const resetPage = () => {
+    setShowingTruck(false);
+    setCreatingTruck(false);
+  };
+
   const createTruck = () => {
-    console.log("Adding truck...");
+    setShowingTruck(false);
+    setCreatingTruck(true);
+    console.log("Creating driver...");
+  };
+
+  const showTruck = () => {
+    setCreatingTruck(false);
+    setShowingTruck(true);
   };
 
   return (
     <PageWithNavigation>
-      <PageHeading>Vehicles</PageHeading>
-      <GapList>
-        <CardButton onClick={createTruck}>+ Add new truck</CardButton>
-        <CardLink to="/">Fleet information</CardLink>
-      </GapList>
+      {!creatingTruck && !showingTruck && (
+        <>
+          <PageHeading>Vehicles</PageHeading>
+          <GapList>
+            <CardButton onClick={createTruck}>+ Add new truck</CardButton>
+            <CardButton onClick={showTruck}>Fleet information</CardButton>
+          </GapList>
+        </>
+      )}
+      {creatingTruck && (
+        <>
+          <PageHeading>
+            <button onClick={resetPage}>&lt; Add a new Truck</button>
+          </PageHeading>
+          <GapList>
+            <CreateTruckForm />
+          </GapList>
+        </>
+      )}
+      {showingTruck && <TrucksList callback={resetPage} />}
     </PageWithNavigation>
   );
 }
