406ee22565177cb78bc3e043c1a979115330528c
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-24 16:35:39 +0200

    add generics to determine typing from passed in endpoint parameter
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -3,29 +3,19 @@ import axios, { AxiosError } from "axios";
 import { Client, Driver, Task, Vehicle } from "../types/ApiResponses";
 import { BASE_API_URL } from "../util/config";
 
-type ApiEndpoints = "drivers" | "clients" | "tasks" | "vehicles";
+type ApiResponseMapping = {
+  drivers: Driver[],
+  clients: Client[],
+  tasks: Task[],
+  vehicles: Vehicle[]
+};
+type ApiEndpoints = keyof ApiResponseMapping;
 
-export default function useApi(endpoint: ApiEndpoints) {
+export default function useApi<T extends ApiEndpoints>(endpoint: T) {
   const fetchData = async () => {
     try {
-      let data;
-      switch (endpoint) {
-        case "drivers":
-          data = await axios.get<Driver[]>(BASE_API_URL + endpoint);
-          break;
-        case "clients":
-          data = await axios.get<Client[]>(BASE_API_URL + endpoint);
-          break;
-        case "tasks":
-          data = await axios.get<Task[]>(BASE_API_URL + endpoint);
-          break;
-        case "vehicles":
-          data = await axios.get<Vehicle[]>(BASE_API_URL + endpoint);
-          break;
-        default:
-          return null;
-      }
-      return data.data;
+      const { data } = await axios.get<ApiResponseMapping[T]>(BASE_API_URL + endpoint);
+      return data;
     } catch (error) {
       if (error instanceof AxiosError != true) {
         console.log("An unknown error ocurred when fetching api:", error);
