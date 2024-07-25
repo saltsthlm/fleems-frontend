ee51de7fc585e3da07f5daa98d60665a9f10a15b
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-25 13:17:49 +0200

    create Throbber component
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

---
+++ src/components/Throbber.tsx
@@ -0,0 +1,9 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+export default function Throbber() {
+  return (
+    <div>
+      <Icon icon="gg:spinner" className="animate-spin text-4xl" />
+    </div>
+  )
+}
