c0951f769fe0acfa56a4092c6a27581979ec49ca
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-25 10:25:53 +0200

    create ProfilePage component
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

---
+++ src/pages/profile/ProfilePage.tsx
@@ -0,0 +1,23 @@
+import Card from "../../components/Card";
+import FormButton from "../../components/FormButton";
+import GapList from "../../components/GapList";
+import PageHeading from "../../components/PageHeading";
+import PageWithNavigation from "../../components/PageWithNavigation";
+
+export default function ProfilePage() {
+  return (
+    <PageWithNavigation>
+      <PageHeading>My Profile</PageHeading>
+      <GapList>
+        <Card className="mb-10">
+          <h1>Supipi Algama</h1>
+          <h2>supipi.algama@gmail.com</h2>
+        </Card>
+        <FormButton className="w-3/5">LOGOUT</FormButton>
+        <FormButton overrideColor className="text-danger w-3/5">
+          DELETE ACCOUNT
+        </FormButton>
+      </GapList>
+    </PageWithNavigation>
+  );
+}
