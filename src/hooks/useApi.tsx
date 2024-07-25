eea8474068386405c6157d311cad3a62233675dc
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-25 13:58:48 +0200

    remove redundant try-catch clause to fix error handling
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { Client, Driver, Task, Vehicle } from "../types/ApiResponses";
 import { BASE_API_URL } from "../util/config";
 
@@ -13,19 +13,10 @@ type ApiEndpoints = keyof ApiResponseMapping;
 
 export default function useApi<T extends ApiEndpoints>(endpoint: T) {
   const fetchData = async () => {
-    try {
-      const { data } = await axios.get<ApiResponseMapping[T]>(
-        BASE_API_URL + endpoint,
-      );
-      return data;
-    } catch (error) {
-      if (error instanceof AxiosError != true) {
-        console.log("An unknown error ocurred when fetching api:", error);
-        return null;
-      }
-      console.log("An error ocurred when fetching api:", error.message);
-      return null;
-    }
+    const { data } = await axios.get<ApiResponseMapping[T]>(
+      BASE_API_URL + endpoint,
+    );
+    return data;
   };
 
   const {
