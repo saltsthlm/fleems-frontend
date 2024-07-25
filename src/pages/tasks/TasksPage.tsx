c0a84a343f8d8e298f72b706f0a883836c6f5dc7
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-25 10:04:07 +0200

    render all tasks on "tasks" page
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/tasks/TasksPage.tsx
+++ src/pages/tasks/TasksPage.tsx
@@ -1,10 +1,25 @@
+import GapList from "../../components/GapList";
 import PageHeading from "../../components/PageHeading";
 import PageWithNavigation from "../../components/PageWithNavigation";
+import useApi from "../../hooks/useApi";
+import Task from "./components/Task";
 
 export default function TasksPage() {
+  const { data, loading } = useApi("tasks");
+
+  if (loading) {
+    return (
+      <PageWithNavigation>
+        <PageHeading>Tasks</PageHeading>
+        <h1>Loading...</h1>
+      </PageWithNavigation>
+    );
+  }
+
   return (
     <PageWithNavigation>
       <PageHeading>Tasks</PageHeading>
+      <GapList>{data?.map((task) => <Task task={task} />)}</GapList>
     </PageWithNavigation>
   );
 }
