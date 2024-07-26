557a19a92e3518e3956cea989d9f2bef74ebe8a7
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-26 16:09:24 +0200

    increase profile picture height on desktop
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/ProfileButton.tsx
+++ src/components/ProfileButton.tsx
@@ -3,8 +3,10 @@ import { Link } from "@tanstack/react-router";
 import { useAuth } from "../AuthProvider";
 import { useEffect, useState } from "react";
 import { PropsWithClassName } from "../types/ComponentTypes";
+import useScreenType from "../hooks/useScreenType";
 
 export default function ProfileButton({ className }: PropsWithClassName) {
+  const { isMobile } = useScreenType();
   const { profile, isLoggedIn } = useAuth();
   const [profilePic, setProfilePic] = useState(null);
   console.log("hi in profile" + profile);
@@ -16,7 +18,7 @@ export default function ProfileButton({ className }: PropsWithClassName) {
   return (
     <Link to="/profile" className={`${className}`}>
       {isLoggedIn ? (
-        <img src={profilePic} alt="user image" className="rounded-full h-8" />
+        <img src={profilePic} alt="user image" className={`rounded-full ${isMobile ? "h-8" : "h-12"}`} />
       ) : (
         <Icon icon="gg:profile" className="h-full [&>g]: text-red" />
       )}
