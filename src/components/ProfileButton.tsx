d0af5ce185943404e800cbfabf1606ef4ca34d2b
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-26 14:06:40 +0200

    allow for className prop in ProfileButton component
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/ProfileButton.tsx
+++ src/components/ProfileButton.tsx
@@ -2,8 +2,9 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 import { Link } from "@tanstack/react-router";
 import { useAuth } from "../AuthProvider";
 import { useEffect, useState } from "react";
+import { PropsWithClassName } from "../types/ComponentTypes";
 
-export default function ProfileButton() {
+export default function ProfileButton({ className }: PropsWithClassName) {
   const { profile, isLoggedIn } = useAuth();
   const [profilePic, setProfilePic] = useState(null);
   console.log("hi in profile" + profile);
@@ -13,11 +14,11 @@ export default function ProfileButton() {
   }, [profile]);
 
   return (
-    <Link to="/profile">
+    <Link to="/profile" className={`${className}`}>
       {isLoggedIn ? (
-        <img src={profilePic} alt="user image" />
+        <img src={profilePic} alt="user image" className="h-full" />
       ) : (
-        <Icon icon="gg:profile" className="h-full" />
+        <Icon icon="gg:profile" className="h-full [&>g]: text-red" />
       )}
     </Link>
   );
