2aa86151398a5d65c3a4d0f04cb99a98f87dd081
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-26 15:30:36 +0200

    remove profile button on desktop for PageHeading
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from "react";
 import ProfileButton from "./ProfileButton";
+import useScreenType from "../hooks/useScreenType";
 
 type PageHeadingProps = {
   noProfileButton?: boolean;
@@ -10,12 +11,14 @@ export default function PageHeading({
   noProfileButton = false,
   sticky = true,
 }: PropsWithChildren<PageHeadingProps>) {
+  const { isMobile } = useScreenType();
+
   return (
     <h1
       className={`py-5 text-xl flex justify-between align-center drop-shadow mb-4 ${sticky && "sticky top-0 z-10 bg-secondary -mx-6 px-6"}`}
     >
       {children}
-      {!noProfileButton && <ProfileButton />}
+      {!noProfileButton && isMobile && <ProfileButton />}
     </h1>
   );
 }
