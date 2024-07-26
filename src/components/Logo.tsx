df9e099bbf4baaed3dd4106ce598f8d0b14e1a42
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-26 13:41:44 +0200

    add additional styling possiblities for Logo component with "size" and "className" props
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,18 @@
 import logo from "../assets/logo.png";
+import { PropsWithClassName } from "../types/ComponentTypes";
 
-export default function Logo() {
+type LogoProps = {
+  size?: "small" | "medium" | "large";
+} & PropsWithClassName
+export default function Logo({ size, className }: LogoProps) {
+  const sizeClasses: { [key: string]: string } = {
+    small: "h-12",
+    medium: "w-46",
+    large: "w-128",
+  }
+
   return (
-    <div className="p-10">
+    <div className={`p-10 ${className} ${size && sizeClasses[size]}`}>
       <img src={logo} className="w-full" />
       <p className="text-center">Trucking made easy</p>
     </div>
