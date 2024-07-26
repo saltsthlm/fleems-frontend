b52177ded7257d14ad5c718efacbe3cad4636d60
adamhedin <adam.s.hedin@gmail.com>
2024-07-26 16:17:50 +0200

    refactor profilebutton
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/ProfileButton.tsx
+++ src/components/ProfileButton.tsx
@@ -1,24 +1,21 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { Link } from "@tanstack/react-router";
 import { useAuth } from "../AuthProvider";
-import { useEffect, useState } from "react";
 import { PropsWithClassName } from "../types/ComponentTypes";
 import useScreenType from "../hooks/useScreenType";
 
 export default function ProfileButton({ className }: PropsWithClassName) {
   const { isMobile } = useScreenType();
   const { profile, isLoggedIn } = useAuth();
-  const [profilePic, setProfilePic] = useState(null);
-  console.log("hi in profile" + profile);
-
-  useEffect(() => {
-    setProfilePic(profile?.picture);
-  }, [profile]);
 
   return (
     <Link to="/profile" className={`${className}`}>
-      {isLoggedIn ? (
-        <img src={profilePic ?? ""} alt="user image" className={`rounded-full ${isMobile ? "h-8" : "h-12"}`} />
+      {isLoggedIn && profile ? (
+        <img
+          src={profile.picture}
+          alt="user image"
+          className={`rounded-full ${isMobile ? "h-8" : "h-12"}`}
+        />
       ) : (
         <Icon icon="gg:profile" className="h-full [&>g]: text-red" />
       )}
