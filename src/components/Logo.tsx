fa55ea475218419cde03848f619e5db739dbd6cc
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-26 15:48:56 +0200

    remove padding on logo
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -12,7 +12,7 @@ export default function Logo({ size, className }: LogoProps) {
   };
 
   return (
-    <div className={`p-10 ${className} ${size && sizeClasses[size]}`}>
+    <div className={`${className} ${size && sizeClasses[size]}`}>
       <img src={logo} className="w-full" />
       <p className="text-center">Trucking made easy</p>
     </div>
