c26e684f97176eb6172520f558f64209243a983a
nelsonJanusson <nelson.janusson@appliedtechnology.se>
2024-07-26 16:37:35 +0200

    i
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>
    Co-authored-by: Adam Hedin <Adam.s.hedin@gmail.com>

--- src/components/RouteMap.tsx
+++ src/components/RouteMap.tsx
@@ -2,20 +2,8 @@ import { MapContainer, Popup, TileLayer } from "react-leaflet";
 import CustomMarker from "./CustomMarker";
 import Route from "./Route";
 import { LegInfoDto } from "../types/ApiResponses";
-//legs: LegInfoDto[]
-export default function RouteMap() {
-  const legs2: LegInfoDto[] = [];
-  const leg1: LegInfoDto = {
-    id: "hi",
-    driver: null,
-    vehicle: null,
-    startTime: null,
-    endTime: null,
-    startLocation: "59.337,18.012",
-    endLocation: "60.589,16.537",
-    distanceDriven: 5,
-  };
-  legs2.push(leg1);
+
+export default function RouteMap(legs: LegInfoDto[]) {
   return (
     <MapContainer center={[61.26, 18.193]} zoom={5} scrollWheelZoom={true}>
       <TileLayer
@@ -25,7 +13,7 @@ export default function RouteMap() {
       <CustomMarker position={[59.337, 18.012]}>
         <Popup>Vehicle ABC123</Popup>
       </CustomMarker>
-      {legs2.map((leg: LegInfoDto) => (
+      {legs.map((leg: LegInfoDto) => (
         <Route source={leg.startLocation} destination={leg.endLocation} />
       ))}
     </MapContainer>
