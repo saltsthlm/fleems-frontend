e9bacce239adb6083638baf4e15be82a12157b1a
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-29 15:34:02 +0200

    change logs route to render top navbar
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/routes/logs.tsx
+++ src/routes/logs.tsx
@@ -1,6 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
-import TripLogs from '../components/TripLogs'
+import { createFileRoute } from "@tanstack/react-router";
+import TripLogs from "../components/TripLogs";
+import PageWithNavigation from "../components/PageWithNavigation";
+import PageHeading from "../components/PageHeading";
 
-export const Route = createFileRoute('/logs')({
-  component: () => <div><TripLogs /></div>
-})
+export const Route = createFileRoute("/logs")({
+  component: () => (
+    <>
+      <PageWithNavigation>
+        <PageHeading>Logs</PageHeading>
+        <TripLogs />
+      </PageWithNavigation>
+    </>
+  ),
+});
