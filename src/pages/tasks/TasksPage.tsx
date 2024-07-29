5e97b498c8463798e78da799050efe79cbdeb7f3
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-29 16:04:40 +0200

    linting
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/tasks/TasksPage.tsx
+++ src/pages/tasks/TasksPage.tsx
@@ -14,8 +14,6 @@
 //   const [searchFilter, setSearchFilter] = useState<string>();
 //   const [activeTab, setActiveTab] = useState<string>('information');
 
-
-
 //   if (isLoading || error) {
 //     return (
 //       <PageWithNavigation>
@@ -44,8 +42,6 @@
 //   );
 // }
 
-
-
 import { useState } from "react";
 import GapList from "../../components/GapList";
 import PageHeading from "../../components/PageHeading";
@@ -58,7 +54,6 @@ import Card from "../../components/Card";
 import SecondaryNavigation from "../../components/SecondaryNavigation";
 import SearchBar from "../../components/SearchBar";
 
-
 type TaskPageProps = {
   callback: () => void;
 };
@@ -66,8 +61,8 @@ export default function TasksPage({ callback }: TaskPageProps) {
   const [searchFilter, setSearchFilter] = useState<string>();
   const [isViewingTask, setIsViewingTask] = useState<boolean>(false);
   const [selectedTask, setSelectedTask] = useState<Task>();
-  const [activeTab, setActiveTab] = useState<string>('information');
-  
+  const [activeTab, setActiveTab] = useState<string>("information");
+
   const { data, isLoading, error } = useApi("tasks");
 
   const viewTask = (task: Task) => {
@@ -79,24 +74,26 @@ export default function TasksPage({ callback }: TaskPageProps) {
     setIsViewingTask(false);
   };
 
- 
   if (isViewingTask && !!selectedTask) {
     return (
       <>
-      <PageWithNavigation>
-        <PageHeading>
-          <button onClick={viewList}>&lt; Task information</button>
-        </PageHeading>
-        
-        <Card className="text-center">
-          <h1 className="text-xl">ABC company</h1>
-          <h2>Route : {selectedTask.startDestination} - {selectedTask.endDestination}</h2>
-          <h2>Task : {selectedTask.payload}</h2>
-          <h2>Status : On going</h2>
-          <h2>No. of legs : {selectedTask.legs?.length}</h2>
-          <h2>Start date : {selectedTask.startDate?.toString()}</h2>
-          <h2>End date : {selectedTask.dateFinished?.toString()}</h2>
-        </Card>
+        <PageWithNavigation>
+          <PageHeading>
+            <button onClick={viewList}>&lt; Task information</button>
+          </PageHeading>
+
+          <Card className="text-center">
+            <h1 className="text-xl">ABC company</h1>
+            <h2>
+              Route : {selectedTask.startDestination} -{" "}
+              {selectedTask.endDestination}
+            </h2>
+            <h2>Task : {selectedTask.payload}</h2>
+            <h2>Status : On going</h2>
+            <h2>No. of legs : {selectedTask.legs?.length}</h2>
+            <h2>Start date : {selectedTask.startDate?.toString()}</h2>
+            <h2>End date : {selectedTask.dateFinished?.toString()}</h2>
+          </Card>
         </PageWithNavigation>
       </>
     );
@@ -106,52 +103,52 @@ export default function TasksPage({ callback }: TaskPageProps) {
     return (
       <>
         <PageWithNavigation>
-        <PageHeading>
-          <button onClick={callback}>&lt; Task information</button>
-        </PageHeading>
-        <GapList>
-          {isLoading && <Throbber />}
-          {error && <h1>An error ocurred: {error.message}</h1>}
-        </GapList>
+          <PageHeading>
+            <button onClick={callback}>&lt; Task information</button>
+          </PageHeading>
+          <GapList>
+            {isLoading && <Throbber />}
+            {error && <h1>An error ocurred: {error.message}</h1>}
+          </GapList>
         </PageWithNavigation>
-        
       </>
     );
   }
 
   return (
     <>
-     <PageWithNavigation>
-      <PageHeading>Tasks</PageHeading>
-      <SecondaryNavigation onTabChange={setActiveTab} activeTab={activeTab} />
-       <SearchBar
+      <PageWithNavigation>
+        <PageHeading>Tasks</PageHeading>
+        <SecondaryNavigation onTabChange={setActiveTab} activeTab={activeTab} />
+        <SearchBar
           value={searchFilter}
           onChange={(e) => setSearchFilter(e.target.value)}
           placeholder="Search tasks"
         />
-      {isLoading && <Throbber />}
-      <GapList>
-        {data?.map((task: Task) => (
-          <CardButtonWithNoStyles
-          key={task.id}
-          onClick={() => viewTask(task)}
-           >
-            <h1 className="text-xl">ABC Company</h1>
-            <div className="flex justify-between w-full text-left">
-              <div> 
-          <h2>Route : {task?.startDestination} - {task?.endDestination}</h2>
-          <h2>Task : {task?.payload}</h2>
-          <h2>Status : On going</h2>
-          <h2>No. of legs : {task?.legs.length}</h2>
-          <h2>Start date : {task?.startDate?.toString()}</h2>
-          <h2>End date : {task?.dateFinished?.toString()}</h2>
+        {isLoading && <Throbber />}
+        <GapList>
+          {data?.map((task: Task) => (
+            <CardButtonWithNoStyles
+              key={task.id}
+              onClick={() => viewTask(task)}
+            >
+              <h1 className="text-xl">ABC Company</h1>
+              <div className="flex justify-between w-full text-left">
+                <div>
+                  <h2>
+                    Route : {task?.startDestination} - {task?.endDestination}
+                  </h2>
+                  <h2>Task : {task?.payload}</h2>
+                  <h2>Status : On going</h2>
+                  <h2>No. of legs : {task?.legs.length}</h2>
+                  <h2>Start date : {task?.startDate?.toString()}</h2>
+                  <h2>End date : {task?.dateFinished?.toString()}</h2>
+                </div>
               </div>
-            </div>
-          </CardButtonWithNoStyles>
-        ))}
-      </GapList>
+            </CardButtonWithNoStyles>
+          ))}
+        </GapList>
       </PageWithNavigation>
     </>
   );
 }
-
