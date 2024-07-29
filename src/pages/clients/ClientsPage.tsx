7c778dc4e349d64e5d86084881f98b7f29df1cfd
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-29 16:00:23 +0200

    fix loading screen, and incorrect prop usage
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/clients/ClientsPage.tsx
+++ src/pages/clients/ClientsPage.tsx
@@ -10,7 +10,6 @@ import Card from "../../components/Card";
 import SecondaryNavigation from "../../components/SecondaryNavigation";
 import SearchBar from "../../components/SearchBar";
 
-
 type ClientsListProps = {
   callback: () => void;
 };
@@ -18,8 +17,8 @@ export default function ClientsList({ callback }: ClientsListProps) {
   const [searchFilter, setSearchFilter] = useState<string>();
   const [isViewingClient, setIsViewingClient] = useState<boolean>(false);
   const [selectedClient, setSelectedClient] = useState<Client>();
-  const [activeTab, setActiveTab] = useState<string>('information');
-  
+  const [activeTab, setActiveTab] = useState<string>("information");
+
   const { data, isLoading, error } = useApi("clients");
 
   const viewClient = (client: Client) => {
@@ -31,15 +30,13 @@ export default function ClientsList({ callback }: ClientsListProps) {
     setIsViewingClient(false);
   };
 
- 
   if (isViewingClient && !!selectedClient) {
     return (
-      <>
       <PageWithNavigation>
         <PageHeading>
           <button onClick={viewList}>&lt; Client information</button>
         </PageHeading>
-        
+
         <Card className="text-center">
           <h1 className="text-xl">{selectedClient.name}</h1>
           <h2>Num of tasks : {selectedClient.tasks.length}</h2>
@@ -50,14 +47,13 @@ export default function ClientsList({ callback }: ClientsListProps) {
           <h2>Email : {selectedClient.contactEmail}</h2>
           <h2>Phone : {selectedClient.contactPhoneNumber}</h2>
         </Card>
-        </PageWithNavigation>
-      </>
+      </PageWithNavigation>
     );
   }
 
   if (isLoading || error) {
     return (
-      <>
+      <PageWithNavigation>
         <PageHeading>
           <button onClick={callback}>&lt; Client information</button>
         </PageHeading>
@@ -65,44 +61,53 @@ export default function ClientsList({ callback }: ClientsListProps) {
           {isLoading && <Throbber />}
           {error && <h1>An error ocurred: {error.message}</h1>}
         </GapList>
-      </>
+      </PageWithNavigation>
     );
   }
 
   return (
-    <>
-     <PageWithNavigation>
+    <PageWithNavigation>
       <PageHeading>Clients</PageHeading>
       <SecondaryNavigation onTabChange={setActiveTab} activeTab={activeTab} />
-       <SearchBar
-          value={searchFilter}
-          onChange={(e) => setSearchFilter(e.target.value)}
-          placeholder="Search clients"
-        />
+      <SearchBar value={searchFilter} callback={setSearchFilter} />
       {isLoading && <Throbber />}
       <GapList>
         {data?.map((client: Client) => (
           <CardButtonWithNoStyles
-          key={client.id}
-          onClick={() => viewClient(client)}
-           >
+            key={client.id}
+            onClick={() => viewClient(client)}
+          >
             <h1 className="text-xl">{client.name}</h1>
             <div className="flex justify-between w-full text-left">
-              <div> 
-                <h2 >Num of tasks : {client.tasks.length}</h2>
+              <div>
+                <h2>Num of tasks : {client.tasks.length}</h2>
                 <h2>Completed : {client.tasks.length}</h2>
                 <h2>Ongoing : {client.tasks.length}</h2>
               </div>
-              <div >
-                <h2>Contact : {client.contactPerson == null ? "Anders Petterson" : client.contactPerson}</h2>
-                <h2>Email : {client.contactEmail == null ? "12/06/2024" : client.contactEmail}</h2>
-                <h2>Phone : {client.contactPhoneNumber == null ? "0763262839" : client.contactPhoneNumber}</h2>
+              <div>
+                <h2>
+                  Contact :{" "}
+                  {client.contactPerson == null
+                    ? "Anders Petterson"
+                    : client.contactPerson}
+                </h2>
+                <h2>
+                  Email :{" "}
+                  {client.contactEmail == null
+                    ? "12/06/2024"
+                    : client.contactEmail}
+                </h2>
+                <h2>
+                  Phone :{" "}
+                  {client.contactPhoneNumber == null
+                    ? "0763262839"
+                    : client.contactPhoneNumber}
+                </h2>
               </div>
             </div>
           </CardButtonWithNoStyles>
         ))}
       </GapList>
-      </PageWithNavigation>
-    </>
+    </PageWithNavigation>
   );
 }
