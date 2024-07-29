dccaefcb486b6fde06d57effa9f19210ef85f78d
adamhedin <adam.s.hedin@gmail.com>
2024-07-29 14:21:52 +0200

    check if data is undefined
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/vehicles/VehiclesPage.tsx
+++ src/pages/vehicles/VehiclesPage.tsx
@@ -23,7 +23,7 @@ export default function TrucksList({ callback }: TrucksListProps) {
   const [selectedTruck, setSelectedTruck] = useState<Vehicle>();
   const [isShowingPopup, setIsShowingPopup] = useState<boolean>(false);
   const [isEditingTruck, setIsEditingTruck] = useState<boolean>(false);
-  const [activeTab, setActiveTab] = useState<string>('information');
+  const [activeTab, setActiveTab] = useState<string>("information");
   const [searchFilter, setSearchFilter] = useState<string>();
 
   const { data, isLoading, error } = useApi("vehicles");
@@ -46,15 +46,14 @@ export default function TrucksList({ callback }: TrucksListProps) {
   if (isEditingTruck) {
     return (
       <>
-      <PageWithNavigation>
-      <PageHeading>
-          <button onClick={() => setIsEditingTruck(false)}>
-            &lt; Edit truck information
-          </button>
-        </PageHeading>
-        <EditTruckForm />
-      </PageWithNavigation>
-       
+        <PageWithNavigation>
+          <PageHeading>
+            <button onClick={() => setIsEditingTruck(false)}>
+              &lt; Edit truck information
+            </button>
+          </PageHeading>
+          <EditTruckForm />
+        </PageWithNavigation>
       </>
     );
   }
@@ -62,72 +61,71 @@ export default function TrucksList({ callback }: TrucksListProps) {
   if (isViewingTruck && !!selectedTruck) {
     return (
       <>
-      <PageWithNavigation>
-        <PageHeading>
-          <button onClick={viewList}>&lt; Truck information</button>
-        </PageHeading>
+        <PageWithNavigation>
+          <PageHeading>
+            <button onClick={viewList}>&lt; Truck information</button>
+          </PageHeading>
 
-        {isShowingPopup && (
-          <Popup>
-            <Card className="bg-tertiary w-full p-12">
-              <h1 className="text-center">
-                Are you sure you would like to delete this truck?
-              </h1>
-              <div className="flex gap-4 justify-between [&>button]:grow">
-                <FormButton
-                  onClick={() => setIsShowingPopup(false)}
-                  className="text-danger"
-                  overrideColor
-                >
-                  YES
-                </FormButton>
-                <FormButton onClick={() => setIsShowingPopup(false)}>
-                  NO
-                </FormButton>
-              </div>
-            </Card>
-          </Popup>
-        )}
-        <Card className="text-center">
-          <h1 className="text-xl">{selectedTruck.licenseNumber}</h1>
-          <h2>Model: {selectedTruck.model}</h2>
-          <h2>Payload: {selectedTruck.payload}</h2>
-          <h2>Weight: {selectedTruck.weight}</h2>
-          <h2>Height: {selectedTruck.height}</h2>
-          <h2>Length: {selectedTruck.length}</h2>
-        </Card>
-        <div className="flex flex-col py-7 gap-4 items-center">
-          <FormButton
-            onClick={() => editTruck(selectedTruck)}
-            className="w-3/5"
-          >
-            EDIT
-          </FormButton>
-          <FormButton
-            onClick={() => setIsShowingPopup(true)}
-            className="w-3/5 text-danger"
-            overrideColor
-          >
-            DELETE
-          </FormButton>
-        </div>
+          {isShowingPopup && (
+            <Popup>
+              <Card className="bg-tertiary w-full p-12">
+                <h1 className="text-center">
+                  Are you sure you would like to delete this truck?
+                </h1>
+                <div className="flex gap-4 justify-between [&>button]:grow">
+                  <FormButton
+                    onClick={() => setIsShowingPopup(false)}
+                    className="text-danger"
+                    overrideColor
+                  >
+                    YES
+                  </FormButton>
+                  <FormButton onClick={() => setIsShowingPopup(false)}>
+                    NO
+                  </FormButton>
+                </div>
+              </Card>
+            </Popup>
+          )}
+          <Card className="text-center">
+            <h1 className="text-xl">{selectedTruck.licenseNumber}</h1>
+            <h2>Model: {selectedTruck.model}</h2>
+            <h2>Payload: {selectedTruck.payload}</h2>
+            <h2>Weight: {selectedTruck.weight}</h2>
+            <h2>Height: {selectedTruck.height}</h2>
+            <h2>Length: {selectedTruck.length}</h2>
+          </Card>
+          <div className="flex flex-col py-7 gap-4 items-center">
+            <FormButton
+              onClick={() => editTruck(selectedTruck)}
+              className="w-3/5"
+            >
+              EDIT
+            </FormButton>
+            <FormButton
+              onClick={() => setIsShowingPopup(true)}
+              className="w-3/5 text-danger"
+              overrideColor
+            >
+              DELETE
+            </FormButton>
+          </div>
         </PageWithNavigation>
       </>
-      
     );
   }
 
   if (isLoading || error) {
     return (
       <>
-      <PageWithNavigation>
-        <PageHeading>
-          <button onClick={callback}>&lt; Truck information</button>
-        </PageHeading>
-        <GapList>
-          {isLoading && <Throbber />}
-          {error && <h1>An error ocurred: {error.message}</h1>}
-        </GapList>
+        <PageWithNavigation>
+          <PageHeading>
+            <button onClick={callback}>&lt; Truck information</button>
+          </PageHeading>
+          <GapList>
+            {isLoading && <Throbber />}
+            {error && <h1>An error ocurred: {error.message}</h1>}
+          </GapList>
         </PageWithNavigation>
       </>
     );
@@ -135,35 +133,34 @@ export default function TrucksList({ callback }: TrucksListProps) {
 
   return (
     <>
-    <PageWithNavigation>
-      <PageHeading>
-        <button onClick={callback}>&lt; Truck information</button>
-      </PageHeading>
-      <SecondaryNavigation onTabChange={setActiveTab} activeTab={activeTab} />
-      <SearchBar
+      <PageWithNavigation>
+        <PageHeading>
+          <button onClick={callback}>&lt; Truck information</button>
+        </PageHeading>
+        <SecondaryNavigation onTabChange={setActiveTab} activeTab={activeTab} />
+        <SearchBar
           value={searchFilter}
           onChange={(e) => setSearchFilter(e.target.value)}
           placeholder="Search trucks"
         />
-      <GapList>
-        {data?.map((truck) => (
-          <CardButton
-            key={truck.id}
-            className="flex flex-row text-left"
-            isCentered={false}
-            onClick={() => viewTruck(truck)}
-          >
-            <div>
-              <h1 className="text-xl">{truck.licenseNumber}</h1>
-              <p>Model: {truck.model}</p>
-              <p>Payload: {truck.payload}</p>
-            </div>
-          </CardButton>
-        ))}
-      </GapList>
-      <Table data={data} />
+        <GapList>
+          {data?.map((truck) => (
+            <CardButton
+              key={truck.id}
+              className="flex flex-row text-left"
+              isCentered={false}
+              onClick={() => viewTruck(truck)}
+            >
+              <div>
+                <h1 className="text-xl">{truck.licenseNumber}</h1>
+                <p>Model: {truck.model}</p>
+                <p>Payload: {truck.payload}</p>
+              </div>
+            </CardButton>
+          ))}
+        </GapList>
+        {data !== undefined && <Table data={data} />}
       </PageWithNavigation>
     </>
   );
 }
-
