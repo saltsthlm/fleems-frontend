6ebcaead41200c5575a64ae8ff27c3ec7b2ea6e4
supipis <nirmanialgama@gmail.com>
2024-07-29 11:41:07 +0200

    client page view is done
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>

--- src/pages/clients/ClientsPage.tsx
+++ src/pages/clients/ClientsPage.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Card from "../../components/Card";
 import GapList from "../../components/GapList";
 import PageHeading from "../../components/PageHeading";
 import PageWithNavigation from "../../components/PageWithNavigation";
@@ -7,26 +6,78 @@ import useApi from "../../hooks/useApi";
 import { Client } from "../../types/ApiResponses";
 import { Icon } from "@iconify/react";
 import Throbber from "../../components/Throbber";
+import CardButtonWithNoStyles from "../../components/CardButtonWithNoStyles";
+import Card from "../../components/Card";
 
-export default function ClientsPage() {
+
+type ClientsListProps = {
+  callback: () => void;
+};
+export default function ClientsList({ callback }: ClientsListProps) {
   const [searchFilter, setSearchFilter] = useState<string>();
+  const [isViewingClient, setIsViewingClient] = useState<boolean>(false);
+  const [selectedClient, setSelectedClient] = useState<Client>();
+  // const [isShowingPopup, setIsShowingPopup] = useState<boolean>(false);
+  // const [isEditingTruck, setIsEditingTruck] = useState<boolean>(false);
 
   const { data, isLoading, error } = useApi("clients");
 
+  // const editTruck = (vehicle: Vehicle) => {
+  //   setIsEditingTruck(true);
+  //   setIsShowingPopup(false);
+  //   console.log(vehicle);
+  // };
+
+  const viewClient = (client: Client) => {
+    setSelectedClient(client);
+    setIsViewingClient(true);
+  };
+
+  const viewList = () => {
+    setIsViewingClient(false);
+  };
+
+ 
+  if (isViewingClient && !!selectedClient) {
+    return (
+      <>
+      <PageWithNavigation>
+        <PageHeading>
+          <button onClick={viewList}>&lt; Client information</button>
+        </PageHeading>
+        
+        <Card className="text-center">
+          <h1 className="text-xl">{selectedClient.name}</h1>
+          <h2>Num of tasks : {selectedClient.tasks.length}</h2>
+          <h2>Completed : {selectedClient.tasks.length}</h2>
+          <h2>On going : {selectedClient.tasks.length}</h2>
+          <h2>Unassigned : {selectedClient.tasks.length}</h2>
+          <h2>Contact person : {selectedClient.contactPerson}</h2>
+          <h2>Email : {selectedClient.contactEmail}</h2>
+          <h2>Phone : {selectedClient.contactPhoneNumber}</h2>
+        </Card>
+        </PageWithNavigation>
+      </>
+    );
+  }
+
   if (isLoading || error) {
     return (
-      <PageWithNavigation>
-        <PageHeading>Clients</PageHeading>
+      <>
+        <PageHeading>
+          <button onClick={callback}>&lt; Client information</button>
+        </PageHeading>
         <GapList>
           {isLoading && <Throbber />}
           {error && <h1>An error ocurred: {error.message}</h1>}
         </GapList>
-      </PageWithNavigation>
+      </>
     );
   }
 
   return (
-    <PageWithNavigation>
+    <>
+     <PageWithNavigation>
       <PageHeading>Clients</PageHeading>
       <div className="pb-3 flex justify-end">
         <div className="bg-button rounded-full py-1 px-3 flex">
@@ -48,23 +99,27 @@ export default function ClientsPage() {
       {isLoading && <Throbber />}
       <GapList>
         {data?.map((client: Client) => (
-          <Card key={client.id}>
+          <CardButtonWithNoStyles
+          key={client.id}
+          onClick={() => viewClient(client)}
+           >
             <h1 className="text-xl">{client.name}</h1>
-            <div className="flex justify-between">
-              <div>
-                <h2>Num of tasks : {client.tasks.length}</h2>
+            <div className="flex justify-between w-full text-left">
+              <div> 
+                <h2 >Num of tasks : {client.tasks.length}</h2>
                 <h2>Completed : {client.tasks.length}</h2>
                 <h2>Ongoing : {client.tasks.length}</h2>
               </div>
-              <div>
+              <div >
                 <h2>Contact : {client.contactPerson == null ? "Anders Petterson" : client.contactPerson}</h2>
                 <h2>Email : {client.contactEmail == null ? "12/06/2024" : client.contactEmail}</h2>
                 <h2>Phone : {client.contactPhoneNumber == null ? "0763262839" : client.contactPhoneNumber}</h2>
               </div>
             </div>
-          </Card>
+          </CardButtonWithNoStyles>
         ))}
       </GapList>
-    </PageWithNavigation>
+      </PageWithNavigation>
+    </>
   );
 }
