4b669e2f678355e215289c25c4f65a671fb4a023
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-30 13:36:48 +0200

    fix map's height on mobile layout
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/TaskView.tsx
+++ src/components/TaskView.tsx
@@ -10,7 +10,7 @@ export default function TaskView({ task }: TaskViewProps) {
     <div className="grid grid-rows-[1fr_auto] gap-5">
       {task ? (
         <>
-          <RouteMap legs={task.legs} className="w-50" />
+          <RouteMap legs={task.legs} className="min-h-80" />
           <CardHarsh className="text-2xl h-min self-end">
             <p>Driver: {task.legs[0]?.driver?.name}</p>
             <p>Vehicle: {task.legs[0]?.vehicle?.licenseNumber}</p>
