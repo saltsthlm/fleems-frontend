40b704462da16e912239a970557c4bf5747f6441
nelsonJanusson <nelson.janusson@appliedtechnology.se>
2024-07-30 16:44:26 +0200

    changed graph types
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>
    Co-authored-by: Adam Hedin <Adam.s.hedin@gmail.com>

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -5,7 +5,7 @@ import PageHeading from "../../components/PageHeading";
 import PageWithNavigation from "../../components/PageWithNavigation";
 import TopDriversStats from "./components/TopDriversStats";
 import Map from "../../components/Map";
-import { BarChart, PieChart } from "@mui/x-charts";
+import { LineChart, PieChart } from "@mui/x-charts";
 
 export default function HomePage() {
   return (
@@ -23,9 +23,13 @@ export default function HomePage() {
         width={400}
         height={200}
       />
-      <BarChart
-        xAxis={[{ scaleType: "band", data: ["group A", "group B", "group C"] }]}
-        series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+      <LineChart
+        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        series={[
+          {
+            data: [2, 5.5, 2, 8.5, 1.5, 5],
+          },
+        ]}
         width={500}
         height={300}
       />
