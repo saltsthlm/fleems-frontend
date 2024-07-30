5d76d42cd6f19c5380dfcd7f4cf9c7f7dd189739
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-30 16:10:40 +0200

    add desktopColumns prop
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/GapList.tsx
+++ src/components/GapList.tsx
@@ -2,15 +2,19 @@ import { PropsWithChildren } from "react";
 import useScreenType from "../hooks/useScreenType";
 import { PropsWithClassName } from "../types/ComponentTypes";
 
+type GapListProps = {
+  desktopColumns: number;
+} & PropsWithClassName;
 export default function GapList({
   children,
   className,
-}: PropsWithChildren & PropsWithClassName) {
+  desktopColumns,
+}: PropsWithChildren<GapListProps>) {
   const { isMobile } = useScreenType();
 
   return (
     <div
-      className={`${isMobile ? "flex flex-col gap-5 items-center" : "grid gap-5"} ${className && className}`}
+      className={`${isMobile ? "flex flex-col gap-5 items-center" : `grid grid-cols-${desktopColumns} gap-5`} ${className && className}`}
     >
       {children}
     </div>
