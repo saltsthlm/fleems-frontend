f8262c1cb960f0ac7d9bba18eeceae2402adec5b
supipis <nirmanialgama@gmail.com>
2024-07-30 09:29:48 +0200

    implement task page search
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>

--- src/pages/tasks/TasksPage.tsx
+++ src/pages/tasks/TasksPage.tsx
@@ -1,48 +1,4 @@
-// import { useState } from "react";
-// import GapList from "../../components/GapList";
-// import PageHeading from "../../components/PageHeading";
-// import PageWithNavigation from "../../components/PageWithNavigation";
-// import SearchBar from "../../components/SearchBar";
-// import SecondaryNavigation from "../../components/SecondaryNavigation";
-// import Throbber from "../../components/Throbber";
-// import useApi from "../../hooks/useApi";
-// import Task from "./components/Task";
-
-// export default function TasksPage() {
-//   const { data, isLoading, error } = useApi("tasks");
-
-//   const [searchFilter, setSearchFilter] = useState<string>();
-//   const [activeTab, setActiveTab] = useState<string>('information');
-
-//   if (isLoading || error) {
-//     return (
-//       <PageWithNavigation>
-//         <PageHeading>Tasks</PageHeading>
-//         <GapList>
-//           {isLoading && <Throbber />}
-//           {error && <h1>An error ocurred: {error.message}</h1>}
-//         </GapList>
-//       </PageWithNavigation>
-//     );
-//   }
-
-//   return (
-//     <PageWithNavigation>
-//       <PageHeading>Tasks</PageHeading>
-//       <SecondaryNavigation onTabChange={setActiveTab} activeTab={activeTab} />
-//       <SearchBar
-//           value={searchFilter}
-//           onChange={(e) => setSearchFilter(e.target.value)}
-//           placeholder="Search tasks"
-//         />
-//       <GapList>
-//         {data?.map((task) => <Task task={task} key={task.id} />)}
-//       </GapList>
-//     </PageWithNavigation>
-//   );
-// }
-
-import { useState } from "react";
+import { useState , useMemo } from "react";
 import GapList from "../../components/GapList";
 import PageHeading from "../../components/PageHeading";
 import PageWithNavigation from "../../components/PageWithNavigation";
@@ -55,12 +11,23 @@ import SecondaryNavigation from "../../components/SecondaryNavigation";
 import SearchBar from "../../components/SearchBar";
 
 export default function TasksPage() {
-  const [searchFilter, setSearchFilter] = useState<string>();
+  const [searchFilter, setSearchFilter] = useState<string>("");
   const [isViewingTask, setIsViewingTask] = useState<boolean>(false);
   const [selectedTask, setSelectedTask] = useState<Task>();
   const [activeTab, setActiveTab] = useState<string>("information");
 
   const { data, isLoading, error } = useApi("tasks");
+
+  const filteredData = useMemo(() => {
+    if (!searchFilter) return data;
+    const lowerCaseFilter = searchFilter.toLowerCase();
+    return data?.filter(task =>
+      task.client.name.toLowerCase().includes(lowerCaseFilter) ||
+      task.startDestination.toLowerCase().includes(lowerCaseFilter) ||
+      task.endDestination.toLowerCase().includes(lowerCaseFilter) ||
+      task.payload.toString().toLowerCase().includes(lowerCaseFilter)
+    );
+  }, [data, searchFilter]);
 
   const viewTask = (task: Task) => {
     setSelectedTask(task);
@@ -73,7 +40,6 @@ export default function TasksPage() {
 
   if (isViewingTask && !!selectedTask) {
     return (
-      <>
       <PageWithNavigation>
         <PageHeading>
           <button onClick={viewList}>&lt; Task information</button>
@@ -81,64 +47,56 @@ export default function TasksPage() {
         
         <Card className="text-center">
           <h1 className="text-xl">{selectedTask.client.name}</h1>
-          <h2>Route : {selectedTask.startDestination} - {selectedTask.endDestination}</h2>
-          <h2>Task : {selectedTask.payload}</h2>
-          <h2>Status : On going</h2>
-          <h2>No. of legs : {selectedTask.legs?.length}</h2>
-          <h2>Start date : {selectedTask.startDate?.toString()}</h2>
-          <h2>End date : {selectedTask.dateFinished?.toString()}</h2>
+          <h2>Route: {selectedTask.startDestination} - {selectedTask.endDestination}</h2>
+          <h2>Task: {selectedTask.payload}</h2>
+          <h2>No. of legs: {selectedTask.legs?.length}</h2>
+          <h2>Start date: {selectedTask.startDate?.toString()}</h2>
+          <h2>End date: {selectedTask.dateFinished?.toString()}</h2>
         </Card>
-        </PageWithNavigation>
-      </>
+      </PageWithNavigation>
     );
   }
 
   if (isLoading || error) {
     return (
-      <>
-        <PageWithNavigation>
-          <PageHeading>Tasks</PageHeading>
-          <GapList>
-            {isLoading && <Throbber />}
-            {error && <h1>An error ocurred: {error.message}</h1>}
-          </GapList>
-        </PageWithNavigation>
-      </>
+      <PageWithNavigation>
+        <PageHeading>Tasks</PageHeading>
+        <GapList>
+          {isLoading && <Throbber />}
+          {error && <h1>An error occurred: {error.message}</h1>}
+        </GapList>
+      </PageWithNavigation>
     );
   }
 
   return (
-    <>
-     <PageWithNavigation>
+    <PageWithNavigation>
       <PageHeading>Tasks</PageHeading>
       <SecondaryNavigation onTabChange={setActiveTab} activeTab={activeTab} />
-       <SearchBar
-          value={searchFilter}
-          onChange={(e) => setSearchFilter(e.target.value)}
-          placeholder="Search tasks"
-        />
+      <SearchBar
+        value={searchFilter}
+        callback={setSearchFilter}
+      />
       {isLoading && <Throbber />}
       <GapList>
-        {data?.map((task: Task) => (
+        {filteredData?.map((task: Task) => (
           <CardButtonWithNoStyles
-          key={task.id}
-          onClick={() => viewTask(task)}
-           >
-            <h1 className="text-xl">{task?.client.name}</h1>
+            key={task.id}
+            onClick={() => viewTask(task)}
+          >
+            <h1 className="text-xl">{task.client.name}</h1>
             <div className="flex justify-between w-full text-left">
               <div> 
-          <h2>Route : {task?.startDestination} - {task?.endDestination}</h2>
-          <h2>Task : {task?.payload}</h2>
-          <h2>Status : On going</h2>
-          <h2>No. of legs : {task?.legs.length}</h2>
-          <h2>Start date : {task?.startDate?.toString()}</h2>
-          <h2>End date : {task?.dateFinished?.toString()}</h2>
+                <h2>Route: {task.startDestination} - {task.endDestination}</h2>
+                <h2>Task: {task.payload}</h2>
+                <h2>No. of legs: {task.legs.length}</h2>
+                <h2>Start date: {task.startDate?.toString()}</h2>
+                <h2>End date: {task.dateFinished?.toString()}</h2>
               </div>
             </div>
           </CardButtonWithNoStyles>
         ))}
       </GapList>
-      </PageWithNavigation>
-    </>
+    </PageWithNavigation>
   );
 }
