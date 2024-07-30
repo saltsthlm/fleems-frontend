69d2d0d3fcbba403fdd2262b93b3caf45ee508d7
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-30 16:10:31 +0200

    improve desktop styling for gaplist
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/GapList.tsx
+++ src/components/GapList.tsx
@@ -1,12 +1,16 @@
 import { PropsWithChildren } from "react";
 import useScreenType from "../hooks/useScreenType";
+import { PropsWithClassName } from "../types/ComponentTypes";
 
-export default function GapList({ children }: PropsWithChildren) {
+export default function GapList({
+  children,
+  className,
+}: PropsWithChildren & PropsWithClassName) {
   const { isMobile } = useScreenType();
 
   return (
     <div
-      className={`${isMobile ? "flex flex-col gap-5 items-center" : "grid grid-cols-4 gap-5"}`}
+      className={`${isMobile ? "flex flex-col gap-5 items-center" : "grid gap-5"} ${className && className}`}
     >
       {children}
     </div>
