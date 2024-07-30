3ab746f1557034a91c843ed75ef5dcc71f0dc172
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-30 15:24:52 +0200

    remove regular "logs" route
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/routes/logs.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import LogPage from "../pages/log/LogPage";
-
-export const Route = createFileRoute("/logs")({
-  component: () => <LogPage />,
-});
