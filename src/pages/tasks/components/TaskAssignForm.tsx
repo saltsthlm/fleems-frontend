4229bce6fdbcbb30a66a8f838b60284321f01a12
supipis <nirmanialgama@gmail.com>
2024-07-30 14:32:49 +0200

    fix the assign task form
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>

--- src/pages/tasks/components/TaskAssignForm.tsx
+++ src/pages/tasks/components/TaskAssignForm.tsx
@@ -3,19 +3,20 @@ import FormWithButton from "../../../components/FormWithButton";
 import { Task } from "../../../types/ApiResponses";
 
 type FormData = {
-  task: number; 
+  task: string; 
   driver: string;
   licensePlate: string;
   startDate?: Date;
   endDate?: Date;
   route: string;
+  client: string;
 };
 
 type TaskAssignFormProps = {
   onSubmit: (data: FormData) => void;
   buttonText: string;
-  initialTask?: Task; 
-  drivers: string[]; 
+  initialTask?: Task;
+  drivers: string[];
 };
 
 export default function TaskAssignForm({
@@ -24,19 +25,15 @@ export default function TaskAssignForm({
   initialTask,
   drivers,
 }: TaskAssignFormProps) {
-  const [task, setTask] = useState<number>(initialTask?.payload ?? 0); // Assuming payload is a number
-  const [driver, setDriver] = useState<string>(initialTask?.driver ?? ''); // Default to empty string if driver is undefined
-  const [licensePlate, setLicensePlate] = useState<string>(initialTask?.licensePlate ?? ''); // Default to empty string if licensePlate is undefined
-  // const [startDate, setStartDate] = useState<string>(initialTask?.startDate ? initialTask.startDate.toISOString().split('T')[0] : ''); // Format as YYYY-MM-DD
-  // const [endDate, setEndDate] = useState<string>(initialTask?.endDate ? initialTask.endDate.toISOString().split('T')[0] : ''); // Format as YYYY-MM-DD
-   const [route, setRoute] = useState<string>(initialTask ? `${initialTask.startDestination} - ${initialTask.endDestination}` : '');
+  const [task, setTask] = useState<string>(initialTask ? `Transportation of ${initialTask.payload} ${initialTask.product}` : ''); 
+  const [driver, setDriver] = useState<string>(initialTask?.driver ?? '');
+  const [licensePlate, setLicensePlate] = useState<string>(initialTask?.licensePlate ?? '');
+  const [route, setRoute] = useState<string>(initialTask ? `${initialTask.startAddress.city} - ${initialTask.endAddress.city}` : '');
+  const [client , setClient] = useState<string>(initialTask?.client.name ?? '');
 
- 
   useEffect(() => {
     if (initialTask) {
-      // setStartDate(initialTask.startDate ? initialTask.startDate.toISOString().split('T')[0] : '');
-      // setEndDate(initialTask.endDate ? initialTask.endDate.toISOString().split('T')[0] : '');
-      setRoute(`${initialTask.startDestination} - ${initialTask.endDestination}`);
+      setRoute(`${initialTask.startAddress.city} - ${initialTask.endAddress.city}`);
     }
   }, [initialTask]);
 
@@ -47,28 +44,35 @@ export default function TaskAssignForm({
       task,
       driver,
       licensePlate,
-      // startDate: startDate ? new Date(startDate) : undefined,
-      // endDate: endDate ? new Date(endDate) : undefined,
       route,
+      client
     };
 
-    callback(formData); 
+    callback(formData);
   };
 
   return (
     <FormWithButton buttonText={buttonText} onSubmit={handleSubmit}>
+      <label htmlFor="client">Client</label>
+      <input
+        name="client"
+        type="text"
+        value={client}
+        onChange={(e) => setClient(e.target.value)}
+      />
       <label htmlFor="task">Task</label>
       <input
         name="task"
-        type="number"
+        type="text"
         value={task}
-        onChange={(e) => setTask(Number(e.target.value))}
+        onChange={(e) => setTask(e.target.value)}
       />
       <label htmlFor="driver">Driver</label>
       <select
         name="driver"
         value={driver}
         onChange={(e) => setDriver(e.target.value)}
+        className="bg-white text-black p-2 w-full gap-3 rounded-xl"
       >
         <option value="">Select driver</option>
         {drivers.map((driverName) => (
@@ -82,20 +86,6 @@ export default function TaskAssignForm({
         value={licensePlate}
         onChange={(e) => setLicensePlate(e.target.value)}
       />
-      <label htmlFor="startDate">Start date</label>
-      {/* <input
-        name="startDate"
-        type="date"
-        value={startDate}
-        onChange={(e) => setStartDate(e.target.value)}
-      /> */}
-      <label htmlFor="endDate">End date</label>
-      {/* <input
-        name="endDate"
-        type="date"
-        value={endDate}
-        onChange={(e) => setEndDate(e.target.value)}
-      /> */}
       <label htmlFor="route">Route</label>
       <input
         name="route"
