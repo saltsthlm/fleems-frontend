88fa5ceda196e13cabe218333ed2cd18ef6f7d98
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-31 08:47:48 +0200

    move stats to separate components
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,41 +1,16 @@
-import FleetDataStats from "./components/FleetDataStats";
-import FleetEventsStats from "./components/FleetEventsStats";
 import GapList from "../../components/GapList";
 import PageHeading from "../../components/PageHeading";
 import PageWithNavigation from "../../components/PageWithNavigation";
-import TopDriversStats from "./components/TopDriversStats";
 import Map from "../../components/Map";
-import { LineChart, PieChart } from "@mui/x-charts";
+import AssignmentStats from "./components/AssignmentStats";
+import FleetDataStats from "./components/FleetDataStats";
 
 export default function HomePage() {
   return (
     <PageWithNavigation>
       <PageHeading>Dashboard</PageHeading>
-      <PieChart
-        series={[
-          {
-            data: [
-              { id: 0, value: 10, label: "Assigned" },
-              { id: 1, value: 15, label: "Unassigned" },
-            ],
-          },
-        ]}
-        width={400}
-        height={200}
-      />
-      <LineChart
-        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-        series={[
-          {
-            data: [2, 5.5, 2, 8.5, 1.5, 5],
-          },
-        ]}
-        width={500}
-        height={300}
-      />
-      <GapList className="grid-cols-4">
-        <FleetEventsStats />
-        <TopDriversStats />
+      <GapList className="grid-cols-3">
+        <AssignmentStats />
         <FleetDataStats />
       </GapList>
       <Map />
