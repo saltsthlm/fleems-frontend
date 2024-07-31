ba585018326603d7338e6d7ff3c03fe01efa24b5
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-31 16:04:27 +0200

    add "disabled" prop
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/components/FormButton.tsx
+++ src/components/FormButton.tsx
@@ -4,16 +4,19 @@ type FormButtonProps = {
   onClick?: () => unknown;
   className?: string;
   overrideColor?: boolean;
+  disabled?: boolean;
 };
 export default function FormButton({
   children,
   onClick: callback,
   className,
   overrideColor,
+  disabled = false,
 }: PropsWithChildren<FormButtonProps>) {
   return (
     <button
       onClick={callback}
+      disabled={disabled}
       className={`btn bg-button border-0 px-8 max-w-48 hover:bg-gray-300  ${className} ${!overrideColor && "text-font"}`}
     >
       {children}
