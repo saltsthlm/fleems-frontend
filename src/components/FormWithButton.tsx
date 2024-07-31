c92fdb3219958d35809265795330587a3ac703d0
supipis <nirmanialgama@gmail.com>
2024-07-31 11:26:33 +0200

    fix the width of the desktop view card
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>

--- src/components/FormWithButton.tsx
+++ src/components/FormWithButton.tsx
@@ -1,20 +1,24 @@
 import { PropsWithChildren, SyntheticEvent } from "react";
 import FormButton from "./FormButton";
 import Card from "./Card";
+import useScreenType from "../hooks/useScreenType";
 
 type FormWithButtonProps = {
   onSubmit?: (arg0: SyntheticEvent) => unknown;
   buttonText: string;
 };
+
 export default function FormWithButton({
   children,
   onSubmit: callback,
   buttonText,
 }: PropsWithChildren<FormWithButtonProps>) {
+  const { isMobile } = useScreenType();
+  
   return (
     <form
       onSubmit={callback}
-      className="flex flex-col gap-3 items-center [&>div>input]:rounded-xl [&>div>input]:p-2"
+      className={`flex flex-col gap-3 items-center [&>div>input]:rounded-xl [&>div>input]:p-2 ${isMobile ? "" : "w-2/5 mx-auto"}`}
     >
       <Card>{children}</Card>
       <div className="w-full flex flex-col items-center gap-3 max-w-48">
