1179044006d07b32d1f7235902528ac749317b72
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-31 08:47:35 +0200

    change contents to linechart from home page
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/home/components/FleetDataStats.tsx
+++ src/pages/home/components/FleetDataStats.tsx
@@ -1,10 +1,19 @@
+import { LineChart } from "@mui/x-charts";
 import Card from "../../../components/Card";
 
 export default function FleetDataStats() {
   return (
     <Card>
-      <h2>Fleet status</h2>
-      <p>Graph + data here...</p>
+      <h1 className="text-xl">Super awesome data</h1>
+      <LineChart
+        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        series={[
+          {
+            data: [2, 5.5, 2, 8.5, 1.5, 5],
+          },
+        ]}
+        height={300}
+      />
     </Card>
   );
 }
