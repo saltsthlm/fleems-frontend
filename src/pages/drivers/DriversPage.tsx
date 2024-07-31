e72965d9bac0d4715fc6baead8ab5e49a9df554e
supipis <nirmanialgama@gmail.com>
2024-07-31 16:03:32 +0200

    fix pop up buttons
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>

--- src/pages/drivers/DriversPage.tsx
+++ src/pages/drivers/DriversPage.tsx
@@ -79,7 +79,7 @@ export default function DriversList() {
               <h1 className="text-center">
                 Are you sure you would like to delete this driver?
               </h1>
-              <div className="flex gap-4 justify-between [&>button]:grow">
+              <div className="flex gap-4 justify-center [&>button]:grow">
                 <FormButton
                   onClick={() => setIsShowingPopup(false)}
                   className="text-danger"
