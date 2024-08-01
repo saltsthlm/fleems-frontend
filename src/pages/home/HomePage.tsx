e463e9f38c184acc872ce9d1bfd53eb8056daa95
supipis <nirmanialgama@gmail.com>
2024-08-01 15:17:33 +0200

    fix stats page
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -9,6 +9,7 @@ import TaskStatus from "./components/TaskStatus";
 import SpeedViolations from "./components/SpeedViolations";
 import RestViolations from "./components/RestViolations";
 import FleetMap from "../../components/FleetMap";
+import TopDrivers from "./components/Topdrivers";
 export default function HomePage() {
   return (
     <PageWithNavigation>
@@ -17,9 +18,23 @@ export default function HomePage() {
         <VehicleStatus />
         <DriverStatus />
         <TaskStatus />
+        <div className="w-full">
         <FinishedTaskStats />
-        <SpeedViolations />
-        <RestViolations />
+        <Card className="mt-4">
+          <h1 className="text-xl">Up coming maintainence</h1>
+          <h2 className="text-base">2024/10/22 - Vehicle GHI 789</h2>
+        </Card>
+        </div>
+        
+        <div className="w-full">
+          <Card className="mb-4 w-full">
+            <h1 className="text-xl">Violations</h1>
+            <SpeedViolations />
+            <RestViolations />
+          </Card>
+          <TopDrivers />
+        </div>
+        
         <FleetMap />
       </GapList>
     </PageWithNavigation>
