7e237715b7c9851434fdd0b8b4343faee7123b39
supipis <nirmanialgama@gmail.com>
2024-08-01 10:22:50 +0200

    Add w-full to card in form with button component

--- src/components/FormWithButton.tsx
+++ src/components/FormWithButton.tsx
@@ -20,7 +20,7 @@ export default function FormWithButton({
       onSubmit={callback}
       className={`flex flex-col gap-3 items-center [&>div>input]:rounded-xl [&>div>input]:p-2 ${isMobile ? "" : "w-2/5 mx-auto"}`}
     >
-      <Card>{children}</Card>
+      <Card className="w-full">{children}</Card>
       <div className="w-full flex flex-col items-center gap-3 max-w-48">
         <FormButton>{buttonText}</FormButton>
       </div>
