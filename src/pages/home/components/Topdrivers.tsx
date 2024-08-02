5ba89853f038bcaa30d0ab6e0614ada343fa6747
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-08-02 08:33:26 +0200

    linting
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Adam Hedin <adam.s.hedin@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/pages/home/components/Topdrivers.tsx
+++ src/pages/home/components/Topdrivers.tsx
@@ -3,17 +3,25 @@ import Card from "../../../components/Card";
 // import useScreenType from "../../../hooks/useScreenType";
 
 export default function TopDrivers() {
-//   const { isMobile } = useScreenType();
+  //   const { isMobile } = useScreenType();
 
   return (
     <Card className="w-full">
-        <h1 className="text-xl">Top Performers of the month</h1>
-   <div className="flex"><Icon icon="lucide:move-up" className="text-green-600"/><span className="text-base ml-2">Sara Larsson - 198103089012</span></div>
-   <div className="flex"><Icon icon="lucide:move-down" className="text-yellow-600"/><span className="ml-2 text-base">Ali Ahmed - 198210112345</span></div>
-   <div className="flex"><Icon icon="lucide:move-up" className="text-green-600"/><span className="ml-2 text-base">Emil Svensson - 197810199123</span></div>
-   <div></div>
-   <div></div>
-  </Card>
-);
+      <h1 className="text-xl">Top Performers of the month</h1>
+      <div className="flex">
+        <Icon icon="lucide:move-up" className="text-green-600" />
+        <span className="text-base ml-2">Sara Larsson - 198103089012</span>
+      </div>
+      <div className="flex">
+        <Icon icon="lucide:move-down" className="text-yellow-600" />
+        <span className="ml-2 text-base">Ali Ahmed - 198210112345</span>
+      </div>
+      <div className="flex">
+        <Icon icon="lucide:move-up" className="text-green-600" />
+        <span className="ml-2 text-base">Emil Svensson - 197810199123</span>
+      </div>
+      <div></div>
+      <div></div>
+    </Card>
+  );
 }
-
